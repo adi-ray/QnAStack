a3d99e6b4786072428f132dc78f12b4020f3bd8b
adi-ray <adityadns03@gmail.com>
2025-06-03 03:47:08 +0530

    feat: add Appwrite integration and database setup for QnA application

---
+++ src/models/name.ts
@@ -0,0 +1,6 @@
+export const db = "main-qnastack";
+export const questionCollection = "questions";
+export const answerCollection = "answers";
+export const commentCollection = "comments";
+export const voteCollection = "votes";
+export const attachmentBucket = "attachments-bucket";
---
+++ src/models/server/question.collection.ts
@@ -0,0 +1,33 @@
+import { IndexType, Permission } from "node-appwrite";
+
+import { db, questionCollection } from "../name";
+import { databases } from "./config";
+
+export default async function createQuestionCollection() {
+    // Create Collection
+    await databases.createCollection(db, questionCollection, questionCollection, [
+        Permission.read("any"),
+        Permission.create("users"),
+        Permission.read("users"),
+        Permission.update("users"),
+        Permission.delete("users"),
+    ]);
+    console.log(":: Question Collection Created ::");
+
+    // Create Attributes
+    await Promise.all([
+        databases.createStringAttribute(db, questionCollection, "title", 100, true),
+        databases.createStringAttribute(db, questionCollection, "content", 10000, true),
+        databases.createStringAttribute(db, questionCollection, "authorId", 50, true),
+        databases.createStringAttribute(db, questionCollection, "tags", 50, true, undefined, true),
+        databases.createStringAttribute(db, questionCollection, "attachmentId", 50, false),
+    ]);
+    console.log(":: Question Attributes Created ::");
+
+    // Create Indexes
+    await Promise.all([
+        databases.createIndex(db, questionCollection, "title", IndexType.Fulltext, ["title"], ["asc"]),
+        databases.createIndex(db, questionCollection, "content", IndexType.Fulltext, ["content"], ["asc"]),
+    ]);
+    console.log(":: Question Indexes Created ::");
+}
