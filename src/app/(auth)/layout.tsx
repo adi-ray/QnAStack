a3ce4cb098be320322c63b56a2f74399d325d7de
adi-ray <adityadns03@gmail.com>
2025-06-09 23:58:57 +0530

    chore(ui): refined login and signup page styling

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { BackgroundBeams } from "@/components/ui/background-beams";
 import { useAuthStore } from "@/store/Auth";
 import { useRouter } from "next/navigation";
 import React from "react";
@@ -20,6 +21,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <div className="relative flex min-h-screen flex-col items-center justify-center py-12">
+      <BackgroundBeams />
       <div className="relative">{children}</div>
     </div>
   );
